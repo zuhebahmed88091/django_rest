4cd3350f912855a699f615b7559ed54d772df9ec
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-12-26 14:25:27 +0600

    Add:We have used viewsets and routers.Firstlly we created ArticlleViewSet class and imported viewset and get_object_or_404 and then in the class we wrote def list, def created, def retrieve, def update and def delete methods.In urls.py for binding the methods we import ArticleViewSet class and routers.Initiate router class and register it and add two url patterns.See code for clarification.

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
-from .views import article_list, ArticleAPIView, ArticleDetailsView, GenericAPIView
+from django.urls import path, include
+from .views import article_list, ArticleAPIView, ArticleDetailsView, GenericAPIView, ArticleViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')
 
 urlpatterns = [
     # path("article/", article_list),
+    path("viewset/", include(router.urls)),
+    path("viewset/<int:pk>", include(router.urls)),
     path("article/", ArticleAPIView.as_view()),
     # path("detail/<int:pk>/", article_detail),
     path("detail/<int:id>/", ArticleDetailsView.as_view()),
