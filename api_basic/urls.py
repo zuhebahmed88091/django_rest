39ce5d50e6389c358466644b6714ae261f8791df
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-12-26 12:01:36 +0600

    update:We used class based API views, For that we have to import APIView from django rest_framework then we created ArticeAPIView class which extends from APIView. In that class we created get and post function which are pre-built in nad then we created ArticleDetailsView class same as view and added get_object,PUT,GET.DELETE methods same as previous process.Only we added article = self.get_object id and in try we returned the article data. At last we added urlpatterns in app/urls.py and runserver.

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import article_list,article_detail
+from .views import article_list, ArticleAPIView, ArticleDetailsView
 
 urlpatterns = [
-    path("article/", article_list),
-    path("detail/<int:pk>/", article_detail),
+    # path("article/", article_list),
+    path("article/", ArticleAPIView.as_view()),
+    # path("detail/<int:pk>/", article_detail),
+    path("detail/<int:id>/", ArticleDetailsView.as_view()),
 ]
