d4159bd21e6650adde78d72dac6413c95fe5baff
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-12-26 12:36:34 +0600

    Updated and add Generic view and mixins and our urlpatterns

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import article_list, ArticleAPIView, ArticleDetailsView
+from .views import article_list, ArticleAPIView, ArticleDetailsView, GenericAPIView
 
 urlpatterns = [
     # path("article/", article_list),
     path("article/", ArticleAPIView.as_view()),
     # path("detail/<int:pk>/", article_detail),
     path("detail/<int:id>/", ArticleDetailsView.as_view()),
+    path("generic/article/<int:id>/", GenericAPIView.as_view()),
 ]
